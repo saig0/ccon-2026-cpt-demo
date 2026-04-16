372bef258abfa626768590d5951dc3fe4c26ffb6
Philipp Ossler <philipp.ossler@gmail.com>
2026-04-16 21:33:26 +0200

    feat: Print all customers on startup

--- src/main/java/io/camunda/demo/ProcessOrderApplication.java
+++ src/main/java/io/camunda/demo/ProcessOrderApplication.java
@@ -1,15 +1,18 @@
 package io.camunda.demo;
 
 import io.camunda.client.annotation.Deployment;
+import io.camunda.demo.model.Customer;
+import io.camunda.demo.model.OrderItem;
 import io.camunda.demo.model.Robot;
+import io.camunda.demo.services.CustomerDatabaseService;
 import io.camunda.demo.services.ProductCatalogService;
 import jakarta.annotation.PostConstruct;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
 
 @SpringBootApplication
 @Deployment(resources = "classpath*:/bpmn/**/*.bpmn")
@@ -19,6 +22,7 @@ public class ProcessOrderApplication {
       org.slf4j.LoggerFactory.getLogger(ProcessOrderApplication.class);
 
   @Autowired private ProductCatalogService productCatalogService;
+  @Autowired private CustomerDatabaseService customerDatabaseService;
 
   public static void main(String[] args) {
     SpringApplication.run(ProcessOrderApplication.class, args);
@@ -26,16 +30,26 @@ public class ProcessOrderApplication {
 
   @PostConstruct
   void printDatabaseEntriesForDebugging() {
-    List<Robot> robots = productCatalogService
-            .findAllRobots();
+    List<Robot> robots = productCatalogService.findAllRobots();
     LOGGER.debug("Available robots in the product catalog: {}", robots.size());
-    robots
-        .forEach(
-            robot ->
-                LOGGER.debug(
-                    "Robot: {} (model: {}, intent: {})",
-                    robot.getName(),
-                    robot.getModelId(),
-                    robot.getIntent()));
+    robots.forEach(
+        robot ->
+            LOGGER.debug(
+                "Robot: {} (model: {}, intent: {})",
+                robot.getName(),
+                robot.getModelId(),
+                robot.getIntent()));
+
+    List<Customer> customers = customerDatabaseService.findAllCustomers();
+    LOGGER.debug("Customers in the database: {}", customers.size());
+    customers.forEach(
+        customer ->
+            LOGGER.debug(
+                "Customer: {} (orders: {})",
+                customer.getName(),
+                customer.getOrders().stream()
+                    .flatMap(order -> order.getItems().stream())
+                    .map(OrderItem::getProductReference)
+                    .collect(Collectors.joining(", "))));
   }
 }
