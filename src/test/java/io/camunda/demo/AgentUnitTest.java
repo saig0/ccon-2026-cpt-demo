a651ac47d19a4ffaf6e8286bd0ddfa8f2ecfe2be
Philipp Ossler <philipp.ossler@gmail.com>
2026-04-21 15:55:15 +0200

    test: Add test cases

--- src/test/java/io/camunda/demo/AgentUnitTest.java
+++ src/test/java/io/camunda/demo/AgentUnitTest.java
@@ -1,12 +1,13 @@
 package io.camunda.demo;
 
 import static io.camunda.process.test.api.CamundaAssert.assertThatProcessInstance;
-import static io.camunda.process.test.api.assertions.ElementSelectors.byId;
+import static io.camunda.process.test.api.assertions.ElementSelectors.*;
 import static io.camunda.process.test.api.assertions.JobSelectors.byElementId;
 import static java.util.Map.entry;
 
 import io.camunda.client.CamundaClient;
 import io.camunda.client.api.response.ProcessInstanceEvent;
+import io.camunda.client.api.search.enums.ElementInstanceType;
 import io.camunda.process.test.api.CamundaProcessTestContext;
 import io.camunda.process.test.api.CamundaSpringProcessTest;
 import java.util.Map;
@@ -17,22 +18,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @CamundaSpringProcessTest
-public class CustomerSupportAgentProcessTest {
+public class AgentUnitTest {
 
   private static final String CONVERSATION_ID = "conversation-1";
 
   @Autowired private CamundaClient client;
   @Autowired private CamundaProcessTestContext processTestContext;
 
-  @BeforeEach
-  void setupMocks() {
-    processTestContext.mockJobWorker("send-chat-message").thenComplete();
-  }
+  private ProcessInstanceEvent processInstance;
 
-  @Test
-  void happyPath() {
-    // given
-    final ProcessInstanceEvent processInstance =
+  @BeforeEach
+  void createProcessInstance() {
+    processInstance =
         client
             .newCreateInstanceCommand()
             .bpmnProcessId(CustomerSupportAgentProcess.PROCESS_ID)
@@ -42,33 +39,44 @@ public class CustomerSupportAgentProcessTest {
                     entry("customerName", "Luke"),
                     entry("userRequest", "I have an issue with my robots"),
                     entry("conversationId", CONVERSATION_ID)))
+            .startBeforeElement(CustomerSupportAgentProcess.AD_HOC_SUB_PROCESS_ELEMENT_ID)
             .send()
             .join();
 
-    // when
-    assertThatProcessInstance(processInstance).hasActiveElements(byId("customer-support-agent"));
+    assertThatProcessInstance(processInstance)
+        .hasActiveElements(byId(CustomerSupportAgentProcess.AD_HOC_SUB_PROCESS_ELEMENT_ID));
+  }
 
+  @Test
+  void userReply() {
+    // given
     processTestContext.completeJobOfAdHocSubProcess(
-        byElementId("customer-support-agent"),
+        byElementId(CustomerSupportAgentProcess.AD_HOC_SUB_PROCESS_ELEMENT_ID),
         result ->
             result
                 .activateElement("send-agent-reply")
                 .variable("message", "Don't panic. I will help you."));
 
+    // when
+    processTestContext.completeJob(byElementId("send-agent-reply"));
+
     assertThatProcessInstance(processInstance).isWaitingForMessage("user-message", CONVERSATION_ID);
 
+    final String userReply = "Thanks, that would be great!";
     client
         .newPublishMessageCommand()
         .messageName("user-message")
         .correlationKey(CONVERSATION_ID)
-        .variables(Map.of("message", "Thanks, that would be great!"))
+        .variables(Map.of("message", userReply))
         .send()
         .join();
 
-    processTestContext.completeJobOfAdHocSubProcess(
-        byElementId("customer-support-agent"), result -> result.completionConditionFulfilled(true));
-
     // then
-    assertThatProcessInstance(processInstance).isCompleted();
+    assertThatProcessInstance(processInstance)
+        .isActive()
+        .hasCompletedElementsInOrder(byName("Send agent reply"), byName("User message received"))
+        .hasCompletedElement(
+            byElementType(ElementInstanceType.AD_HOC_SUB_PROCESS_INNER_INSTANCE), 1)
+        .hasVariable("toolCallResult", userReply);
   }
 }
---
+++ src/test/java/io/camunda/demo/CustomerSupportAgentProcess.java
@@ -0,0 +1,7 @@
+package io.camunda.demo;
+
+public class CustomerSupportAgentProcess {
+
+  public static final String PROCESS_ID = "customer-support-agent-process";
+  public static final String AD_HOC_SUB_PROCESS_ELEMENT_ID = "customer-support-agent";
+}
