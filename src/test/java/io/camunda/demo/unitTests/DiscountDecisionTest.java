0401e2e5394bdbece0e4aea17417fd0a558b877f
Philipp Ossler <philipp.ossler@gmail.com>
2026-04-28 15:45:19 +0200

    test: Parameterized test

--- src/test/java/io/camunda/demo/unitTests/DiscountDecisionTest.java
+++ src/test/java/io/camunda/demo/unitTests/DiscountDecisionTest.java
@@ -7,29 +7,40 @@ import io.camunda.client.api.response.EvaluateDecisionResponse;
 import io.camunda.process.test.api.CamundaProcessTestContext;
 import io.camunda.process.test.api.CamundaSpringProcessTest;
 import java.util.Map;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"camunda.client.worker.defaults.enabled=false"})
 @CamundaSpringProcessTest
 public class DiscountDecisionTest {
 
   @Autowired private CamundaClient client;
   @Autowired private CamundaProcessTestContext processTestContext;
 
-  @Test
-  void shouldCalculateDiscount() {
+  @ParameterizedTest
+  @CsvSource({
+    // Purchased robots | Discount
+    "0, 0", //
+    "1, 10", //
+    "2, 10", //
+    "3, 15", //
+    "4, 15", //
+    "5, 25", //
+    "10, 25", //
+  })
+  void shouldCalculateDiscount(final int customerPreviousRobotCount, final int expectedDiscount) {
     // when
     final EvaluateDecisionResponse evaluateDecisionResponse =
         client
             .newEvaluateDecisionCommand()
             .decisionId("discount")
-            .variables(Map.of("customerPreviousRobotCount", 0))
+            .variables(Map.of("customerPreviousRobotCount", customerPreviousRobotCount))
             .send()
             .join();
 
     // then
-    assertThatDecision(evaluateDecisionResponse).hasOutput(0);
+    assertThatDecision(evaluateDecisionResponse).hasOutput(expectedDiscount);
   }
 }
