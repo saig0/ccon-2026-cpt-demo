4560148428adce9daeb71e563991f8238d11a657
Philipp Ossler <philipp.ossler@gmail.com>
2026-05-04 20:43:41 +0200

    test: Improve test cases

--- src/test/java/io/camunda/demo/integrationTests/AgentIntegrationTest.java
+++ src/test/java/io/camunda/demo/integrationTests/AgentIntegrationTest.java
@@ -9,17 +9,10 @@ import io.camunda.demo.util.CustomerSupportAgentProcess;
 import io.camunda.demo.util.CustomerSupportAgentProcessUtil;
 import io.camunda.process.test.api.CamundaProcessTestContext;
 import io.camunda.process.test.api.CamundaSpringProcessTest;
-import io.camunda.process.test.api.mock.JobWorkerMockBuilder.JobWorkerMock;
 import java.time.Duration;
-import java.util.List;
-import java.util.function.Supplier;
-import org.jspecify.annotations.Nullable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtensionContext;
-import org.junit.jupiter.api.extension.RegisterExtension;
-import org.junit.jupiter.api.extension.TestWatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +31,8 @@ import org.springframework.test.context.ActiveProfiles;
 @CamundaSpringProcessTest
 public class AgentIntegrationTest {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(AgentIntegrationTest.class);
+
   private static final Runnable END_CONVERSATION =
       () -> {
         // end the conversation
@@ -48,12 +43,6 @@ public class AgentIntegrationTest {
 
   private CustomerSupportAgentProcessUtil processUtil;
 
-  private JobWorkerMock sendChatMessageMock;
-
-  @RegisterExtension
-  private final ConversationLogger conversationLogger =
-      new ConversationLogger(() -> sendChatMessageMock);
-
   @BeforeEach
   void setup() {
     processUtil =
@@ -63,10 +52,16 @@ public class AgentIntegrationTest {
   @BeforeEach
   void mockJobWorkers() {
     // Complete all send chat message jobs
-    sendChatMessageMock =
-        processTestContext
-            .mockJobWorker(CustomerSupportAgentProcess.SEND_CHAT_MESSAGE_JOB_TYPE)
-            .thenComplete();
+    processTestContext
+        .mockJobWorker(CustomerSupportAgentProcess.SEND_CHAT_MESSAGE_JOB_TYPE)
+        .withHandler(
+            (jobClient, job) -> {
+              LOGGER.info(
+                  "Send agent message: '{}'",
+                  job.getVariable(CustomerSupportAgentProcess.Variables.SEND_CHAT_MESSAGE));
+
+              jobClient.newCompleteCommand(job).send();
+            });
   }
 
   @Test
@@ -102,8 +97,8 @@ public class AgentIntegrationTest {
   }
 
   @Test
-  @DisplayName("Should offer an upgrade to reduce the verbosity if the problem is about C-3PO")
-  void shouldOfferUpgrade() {
+  @DisplayName("Should offer an upgrade and place an order")
+  void shouldOfferUpgradeAndPlaceOrder() {
     // given
     final ProcessInstanceEvent processInstance =
         processUtil.createProcessInstance("Luke", "I have a problem with my robot");
@@ -113,18 +108,21 @@ public class AgentIntegrationTest {
         .when(() -> processUtil.awaitUserMessage(processInstance))
         .as("Mock user reply")
         .then(() -> processUtil.publishUserMessage("It's about C3P0. He is talking too much."))
-        .then(() -> processUtil.publishUserMessage("Perfect. I want to have this upgrade."))
+        .then(() -> processUtil.publishUserMessage("Sounds good. Do I get any discount?"))
+        .then(() -> processUtil.publishUserMessage("Perfect. Please order it for me."))
         .then(END_CONVERSATION);
 
     // then
     assertThatProcessInstance(processInstance)
         .withAssertionTimeout(Duration.ofMinutes(2))
-        .hasCompletedElement(byId(CustomerSupportAgentProcess.SEND_AGENT_REPLY_ELEMENT_ID), 3);
+        .hasCompletedElement(byId(CustomerSupportAgentProcess.SEND_AGENT_REPLY_ELEMENT_ID), 4);
 
     assertThatProcessInstance(processInstance)
         .hasCompletedElements(
             byId(CustomerSupportAgentProcess.LOAD_CUSTOMER_DATA_ELEMENT_ID),
-            byId(CustomerSupportAgentProcess.SEARCH_KNOWLEDGE_BASE_ELEMENT_ID))
+            byId(CustomerSupportAgentProcess.SEARCH_KNOWLEDGE_BASE_ELEMENT_ID),
+            byId(CustomerSupportAgentProcess.CALCULATE_DISCOUNT_ELEMENT_ID),
+            byId(CustomerSupportAgentProcess.ORDER_ITEMS_ELEMENT_ID))
         .hasVariableSatisfiesJudge(
             CustomerSupportAgentProcess.Variables.CONVERSATION,
             """
@@ -132,36 +130,8 @@ public class AgentIntegrationTest {
                       1. A greeting to 'Luke', \
                       2. Ask if the problem is about 'R2-D2' or 'C-3PO', \
                       3. Confirm that this is expected behavior,
-                      4. Offer an upgrade to reduce the verbosity.""");
-  }
-
-  private static final class ConversationLogger implements TestWatcher {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ConversationLogger.class);
-
-    private static final String DELIMITER = "\n=======================================\n";
-
-    private final Supplier<JobWorkerMock> sendChatMessageMockSupplier;
-
-    private ConversationLogger(final Supplier<JobWorkerMock> sendChatMessageMockSupplier) {
-      this.sendChatMessageMockSupplier = sendChatMessageMockSupplier;
-    }
-
-    @Override
-    public void testFailed(final ExtensionContext context, @Nullable final Throwable cause) {
-      final List<String> messages =
-          sendChatMessageMockSupplier.get().getActivatedJobs().stream()
-              .map(
-                  job ->
-                      (String)
-                          job.getVariable(CustomerSupportAgentProcess.Variables.SEND_CHAT_MESSAGE))
-              .toList();
-
-      LOGGER.info(
-          "Test failed. Dumping conversation logs: (size: {}){}{}",
-          messages.size(),
-          DELIMITER,
-          String.join(DELIMITER, messages));
-    }
+                      4. Offer an upgrade to reduce the verbosity,
+                      6. Offer a discount of 15% for the upgrade,
+                      7. Confirm the order of the upgrade.""");
   }
 }
