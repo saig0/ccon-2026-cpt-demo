9b2897586476aee2b8c1cafde84c564bde8dd1b2
Philipp Ossler <philipp.ossler@gmail.com>
2026-05-05 09:57:28 +0200

    test: Merge JSON test cases

--- src/test/java/io/camunda/demo/unitTests/AgentUnitJsonTest.java
+++ src/test/java/io/camunda/demo/unitTests/AgentUnitJsonTest.java
@@ -4,20 +4,26 @@ import io.camunda.process.test.api.CamundaSpringProcessTest;
 import io.camunda.process.test.api.testCases.TestCase;
 import io.camunda.process.test.api.testCases.TestCaseRunner;
 import io.camunda.process.test.api.testCases.TestCaseSource;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-/** JSON-based test cases for the discount decision table. */
-@SpringBootTest(properties = {"camunda.client.worker.defaults.enabled=false"})
+/**
+ * JSON-based unit tests for the customer support agent process. The tests are counterparts of the
+ * Java-based unit tests.
+ */
+@SpringBootTest
 @CamundaSpringProcessTest
-public class DiscountDecisionJsonTest {
+public class AgentUnitJsonTest {
 
   @Autowired private TestCaseRunner testCaseRunner;
 
-  @ParameterizedTest
-  @TestCaseSource(fileNames = "discount-decision-test.json")
+  @ParameterizedTest(name = "[{index}] {0} ({1})")
+  @TestCaseSource(directory = "/test-cases/unit-tests")
+  @DisplayName("Run JSON-based test case for customer support agent process")
   void runJsonTestCase(final TestCase testCase, final String fileName) {
+    // when/then: run and verify the test case
     testCaseRunner.run(testCase);
   }
 }
